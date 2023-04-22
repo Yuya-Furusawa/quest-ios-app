4b3176b8372c40fa4d0ec7235cc53204e0a2f29a
Yuya Furusawa <fullyou0798@gmail.com>
2023-04-22 14:51:36 +0900

    Challengeの表示

---
+++ quest-ios-app/Views/QuestDetail/ChallengeCard.swift
@@ -0,0 +1,46 @@
+//
+//  ChallengeCard.swift
+//  quest-ios-app
+//
+//  Created by 古澤優也 on 2023/04/22.
+//
+
+import SwiftUI
+
+struct ChallengeCard: View {
+    let challenge: Challenge
+    
+    var body: some View {
+        VStack(alignment: .leading){
+            Text(challenge.name)
+                .font(.headline)
+                .fontWeight(.bold)
+                .foregroundColor(Color.black)
+                .multilineTextAlignment(.leading)
+                .padding(.bottom, 1.0)
+            
+            Text(challenge.description)
+                .font(.footnote)
+                .foregroundColor(Color.gray)
+                .multilineTextAlignment(.leading)
+                .lineLimit(2)
+                .truncationMode(.tail)
+                .padding(.bottom, 1.0)
+        }
+        .padding(.all, 16.0)
+        .border(/*@START_MENU_TOKEN@*/Color(red: 0.8980392156862745, green: 0.9058823529411765, blue: 0.9215686274509803)/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/1/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ChallengeCard_Previews: PreviewProvider {
+    static var previews: some View {
+        let challenge = Challenge(
+            id: "1",
+            name: "スターバックス渋谷センター街店に行く",
+            description: "スターバックス渋谷センター街店で買い物をして滞在する",
+            quest_id: "LLi9PEyXneHRrm6Elny1I"
+        )
+        ChallengeCard(challenge: challenge)
+            .padding()
+    }
+}
--- quest-ios-app/Views/QuestDetail/QuestDetail.swift
+++ quest-ios-app/Views/QuestDetail/QuestDetail.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct QuestDetail: View {
     let quest:Quest
-
+    
     var body: some View {
         VStack(alignment: .leading){
             Text(quest.title)
@@ -26,7 +26,11 @@ struct QuestDetail: View {
                 Text("参加人数：\(quest.num_participate)人")
                 Text("クリア人数：\(quest.num_clear)人")
             }
-            Spacer(minLength: 10)
+            Spacer(minLength: 30).fixedSize()
+            
+            ForEach(quest.challenges){ challenge in
+                ChallengeCard(challenge: challenge)
+            }
         }
         .padding()
     }
@@ -34,15 +38,29 @@ struct QuestDetail: View {
 
 struct QuestDetail_Preview: PreviewProvider {
     static var previews: some View {
+        let challenge_1 = Challenge(
+            id: "1",
+            name: "スターバックス渋谷センター街店に行く",
+            description: "スターバックス渋谷センター街店で買い物をして滞在する",
+            quest_id: "LLi9PEyXneHRrm6Elny1I"
+        )
+        
+        let challenge_2 = Challenge(
+            id: "2",
+            name: "STARBUCKS RESERVE ROASTERY TOKYOに行く",
+            description: "STARBUCKS RESERVE ROASTERY TOKYOで買い物をして滞在する",
+            quest_id: "LLi9PEyXneHRrm6Elny1I"
+        )
+        
         let quest = Quest(
-                id: "1",
-                title: "This is a test",
-                description: "This is a test",
-                difficulty: "Normal",
-                num_participate: 12345,
-                num_clear: 678,
-                challenges: []
-            )
+            id: "1",
+            title: "This is a test",
+            description: "This is a test",
+            difficulty: "Normal",
+            num_participate: 12345,
+            num_clear: 678,
+            challenges: [challenge_1, challenge_2]
+        )
         VStack {
             QuestDetail(quest: quest)
             Spacer(minLength: 10)
